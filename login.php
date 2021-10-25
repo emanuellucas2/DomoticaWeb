20aed64cb564f8865ae830802098a8388f88e331
emanuellucas2 <emanuellucas2@yahoo.com.br>
2021-10-25 18:41:19 -0300

    PI version

--- login.php
+++ login.php
@@ -1,8 +1,5 @@
 <?php
-
-	
 	ob_start();
-
 	if(count($_POST)> 0){
 
 		$email = $_POST["email"];
@@ -11,10 +8,8 @@
 		$servername = "localhost";
 		$username = "root";
 		$password = "";
-
 		try {
 	  		$conn = new PDO("mysql:host=$servername;dbname=Domotica", $username, $password);
-	  		// set the PDO error mode to exception
 	  		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	  		
 	  		$stmt = $conn->prepare("SELECT codigo FROM Usuarios WHERE email=:email AND senha=:senha");
@@ -22,41 +17,29 @@
 	  		$stmt->bindParam(':senha',$senha, PDO::PARAM_STR);
 	  		$stmt->execute();
 
-	  		// set the resulting array to associative
 	  		$result = $stmt->fetchAll();
 	  		$qtd = count($result);
 	
-
 	  		if($qtd == 1){
 
 	  			$Usuario_id = $result[0]["codigo"];
 
-
 	  			$stmt2 = $conn->prepare("SELECT * FROM Amostras WHERE Usuario_id=:Usuario_id");
 	  			$stmt2->bindParam(':Usuario_id',$Usuario_id, PDO::PARAM_STR);
 	  			$stmt2->execute();
-
-	  			// set the resulting array to associative
 	  			$result2["amostras"] = $stmt2->fetchAll();
 
 	  			ob_end_clean();
-
 	  			include("usuario.php");
-
 	  		}else{
-
 	  			$resultado["msg"] = "Email e Senha não conferem!";
 	  			$resultado["cod"] = 0;
 	  		}
-
 		} catch(PDOException $e) {
 	  		echo "Connection failed: " . $e->getMessage();
 		}
-
-
 		$conn = NULL;
-
 	}
 
-	//include("index.php");	
+	include("index.php");	
 ?>	
